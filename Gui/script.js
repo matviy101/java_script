7ab8d404580193921b5b6f5cb9436af5d932e449
matviy101 <syskomatviy2@gmail.com>
2025-07-17 14:10:19 +0100

    Javascript hw3

--- Gui/script.js
+++ Gui/script.js
@@ -1,43 +1,80 @@
-let celsius = 25;
-let fahrenheit = (celsius * 9/5) + 32;
-console.log(fahrenheit);
+//let celsius = 25;
+//let fahrenheit = (celsius * 9/5) + 32;
+//console.log(fahrenheit);
+//
+//let daysInMonth = 30;
+//let hoursInMonth = daysInMonth * 24;
+//let minutesInMonth = hoursInMonth * 60;
+//console.log(hoursInMonth);
+//console.log(minutesInMonth);
+//
+//let health = 100;
+//let energy = 80;
+//health -= 20;
+//energy -= 15;
+//console.log(health);
+//console.log(energy);
+//
+//let totalPrice = 150;
+//let discountedPrice = totalPrice * 0.9;
+//console.log(discountedPrice);
+//
+//const floatNumber = 12.78;
+//let roundedDown = Math.floor(floatNumber);
+//console.log(roundedDown);
+//
+//const floatString = "45.67";
+//let parsedFloat = parseFloat(floatString);
+//console.log(parsedFloat);
+//
+//const intString = "123";
+//let parsedInt = parseInt(intString);
+//console.log(parsedInt);
+//
+//let number = 64;
+//let sqrtNumber = Math.sqrt(number);
+//console.log(sqrtNumber);
+//
+//const integer = 42;
+//const stringNumber = "256";
+//let convertedInt = parseInt(stringNumber);
+//console.log(convertedInt);
+//let convertedString = integer.toString();
+//console.log(convertedString);#
 
-let daysInMonth = 30;
-let hoursInMonth = daysInMonth * 24;
-let minutesInMonth = hoursInMonth * 60;
-console.log(hoursInMonth);
-console.log(minutesInMonth);
 
-let health = 100;
-let energy = 80;
-health -= 20;
-energy -= 15;
-console.log(health);
-console.log(energy);
 
-let totalPrice = 150;
-let discountedPrice = totalPrice * 0.9;
-console.log(discountedPrice);
 
-const floatNumber = 12.78;
-let roundedDown = Math.floor(floatNumber);
-console.log(roundedDown);
 
-const floatString = "45.67";
-let parsedFloat = parseFloat(floatString);
-console.log(parsedFloat);
+let result = 5+5+'5';
+console.log(result);
+console.log(typeof result);
 
-const intString = "123";
-let parsedInt = parseInt(intString);
-console.log(parsedInt);
 
-let number = 64;
-let sqrtNumber = Math.sqrt(number);
-console.log(sqrtNumber);
+let email = "syskomatviy2@gmail.com";
 
-const integer = 42;
-const stringNumber = "256";
-let convertedInt = parseInt(stringNumber);
-console.log(convertedInt);
-let convertedString = integer.toString();
-console.log(convertedString);
+let containsAt = email.includes('@');
+
+let length = email.length;
+console.log("Contains '@':", containsAt);
+console.log("Total characters:", length);
+
+
+let my = "My";
+let name = "name";
+let is = "is";
+let fullName = my + " " + name + " " + is + " Viktor";
+
+console.log(fullName);
+
+
+let userName = "Oleksandra";
+let payment = 300;
+
+let message = `Дякую, ${userName}! До сплати ${payment} грн`;
+
+alert(message);
+
+
+
+
